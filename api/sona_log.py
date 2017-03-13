29f19fe3c6c9255a65bae834c9b5fd5fd1d38937
jtdream <sn2@gmail.com>
2017-03-13 09:36:47 +0900

    skeleton base

--- api/sona_log.py
+++ api/sona_log.py
@@ -18,34 +18,41 @@ import traceback
 from logging.handlers import TimedRotatingFileHandler
 from config import CONF
 
+DEFAULT_LOG_PATH = os.getcwd() + "/log/"
+DEFAULT_LOGGER_NAME = 'sona_logger'
+
 
 class Log:
-    LOG = logging.getLogger(__name__)
+    logger = logging.getLogger(DEFAULT_LOGGER_NAME)
 
-    def __init__(self):
-        log_path = os.getcwd() + "/log/"
-        if not os.path.exists(log_path):
-            os.makedirs(log_path)
-        file_name = log_path + CONF.base()['log_file_name']
+    def __init__(self, file_name):
+        if not os.path.exists(DEFAULT_LOG_PATH):
+            os.makedirs(DEFAULT_LOG_PATH)
+        log_file_name = DEFAULT_LOG_PATH + file_name
 
         # formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
-        formatter = logging.Formatter('%(asctime)s %(message)s')
-        handler = TimedRotatingFileHandler(file_name,
+        formatter = logging.Formatter('%(asctime)s.%(msecs)03d %(message)s',
+                                      datefmt='(%m/%d) %H:%M:%S')
+        handler = TimedRotatingFileHandler(log_file_name,
                                            when=CONF.base()['log_rotate_time'],
                                            backupCount=CONF.base()['log_backup_count'])
         handler.setFormatter(formatter)
-        self.LOG.addHandler(handler)
-        self.LOG.setLevel(logging.DEBUG)
+        self.logger.addHandler(handler)
+        self.logger.setLevel(logging.DEBUG)
 
     @classmethod
     def info(cls, message, *args):
-        cls.LOG.info(message % args)
+        cls.logger.info(message % args)
+
+    @classmethod
+    def error(cls, message, *args):
+        cls.logger.error(message % args)
 
     @classmethod
     def exception(cls):
         exc_type, exc_value, exc_traceback = sys.exc_info()
         lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
-        cls.info("%s", ''.join('   | ' + line for line in lines))
+        cls.error("%s", ''.join('   | ' + line for line in lines))
 
 
-LOG = Log()
+LOG = Log(CONF.base()['log_file_name'])
